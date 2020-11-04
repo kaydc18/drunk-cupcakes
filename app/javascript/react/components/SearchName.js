81d6ec97f54b71d60e6522cd57d4d7eb66ca5b3e
kaydc18 <klattk@gmail.com>
2020-11-04 14:27:25 -0500

    more custom css and search forms set up

---
+++ app/javascript/react/components/SearchName.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const SearchIngredient = (props) => {
+  return(
+    <div className="callout-purple">
+      <h2 className="text-center">Search By Ingredient</h2>
+      <form>
+        <label>Alcohol
+          <select>
+            <option value="husker">Vodka</option>
+            <option value="starbuck">Gin</option>
+            <option value="hotdog">Rum</option>
+            <option value="apollo">Tequila</option>
+          </select>
+        </label>
+      </form>
+    </div>
+
+  )
+}
+
+export default SearchIngredient
