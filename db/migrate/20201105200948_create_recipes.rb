2ca365647ec3e87a6685299f989f1638112ce231
kaydc18 <klattk@gmail.com>
2020-11-05 15:59:02 -0500

    added to table and deleted spec file

--- db/migrate/20201105200948_create_recipes.rb
+++ db/migrate/20201105200948_create_recipes.rb
@@ -1,6 +1,8 @@
 class CreateRecipes < ActiveRecord::Migration[5.2]
   def change
     create_table :recipes do |t|
+      t.bigint :id, primary: true
+      t.string :name, null: false
 
       t.timestamps
     end
