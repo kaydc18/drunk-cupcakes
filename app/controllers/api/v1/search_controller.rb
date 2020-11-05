a955cf3b250d50cb2b4ad3d64c03a67ba2cec215
kaydc18 <klattk@gmail.com>
2020-11-05 15:41:40 -0500

    started recipe table and finished up some search pages

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,6 +1,6 @@
 require "faraday"
 
-class HomesController < ApplicationController
+class Api::V1::SearchController < ApplicationController
   def search
     BASE_URL = "https://www.thecocktaildb.com/api/json/v2/#{ENV['COCKTAIL_DB']}/filter.php?"
     response = Faraday.get("#{BASE_URL}i=#{query_1},#{query_2}")
