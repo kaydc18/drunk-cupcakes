254494c5d8a97976df1ff2440fd6426651166709
kaydc18 <klattk@gmail.com>
2020-11-05 19:11:15 -0500

    deleted all user changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :first_name, null: false
-  validates :last_name, null: false
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -2,10 +2,9 @@ require 'factory_bot'
 
 FactoryBot.define do
   factory :user do
-    sequence(:first_name) {|n| "first name #{n}" }
-    sequence(:last_name) {|n| "last name #{n}" }
     sequence(:email) {|n| "user#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
   end
+
 end
