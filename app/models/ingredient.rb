ff86582051d1360dbbde600468be46ff2d058681
kaydc18 <klattk@gmail.com>
2020-11-06 14:31:48 -0500

    created all tables and associations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,7 @@
 class Ingredient < ApplicationRecord
   validates :ingredient_id, presence: true
   validates :ingredient_name, presence: true
+
+  has_many :measurements
+  has_many :recipes, through: :measurements
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ApplicationRecord
   validates :drink_id, presence: true
   validates :drink_name, presence: true
   
+  has_many :measurements
+  has_many :ingredients, through: :measurements
 end
