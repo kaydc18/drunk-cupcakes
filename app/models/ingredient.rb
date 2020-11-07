7a76ed11408d5c0b029ff019f7d4fedf968b621e
kaydc18 <klattk@gmail.com>
2020-11-07 16:52:23 -0500

    created working seed file

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,7 +1,7 @@
 class Ingredient < ApplicationRecord
   validates :ingredient_id, presence: true
   validates :ingredient_name, presence: true
-  validates :ingredient_alcohol, presence: true
+  validates :ingredient_alcohol, inclusion: { in: [true, false] }
 
   has_many :measurements
   has_many :recipes, through: :measurements
