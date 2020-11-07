1f3929fcd2773b169cb46bd6cfcf8a9d29818b9d
kaydc18 <klattk@gmail.com>
2020-11-07 17:03:23 -0500

    trying to fix heroku

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,12 +1,12 @@
 require "faraday"
 
 class Api::V1::SearchController < ApplicationController
-  def search
-    BASE_URL = "https://www.thecocktaildb.com/api/json/v2/#{ENV['COCKTAIL_DB']}/filter.php?"
-    response = Faraday.get("#{BASE_URL}i=#{query_1},#{query_2}")
+  # def search
+  #   BASE_URL = "https://www.thecocktaildb.com/api/json/v2/#{ENV['COCKTAIL_DB']}/filter.php?"
+  #   response = Faraday.get("#{BASE_URL}i=#{query_1},#{query_2}")
 
-    JSON.parse(response.body)
+  #   JSON.parse(response.body)
 
-    binding.pry
-  end
+  #   binding.pry
+  # end
 end
