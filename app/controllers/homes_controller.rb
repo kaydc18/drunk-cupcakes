58379bd5391c94def8eaa6bc389ad135f2f58951
kaydc18 <klattk@gmail.com>
2020-11-15 20:48:33 -0500

    removed the sign in

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   def index
     current_user
-    if current_user == true
+    if current_user === true
     @user_id = current_user.id
     end
   end
