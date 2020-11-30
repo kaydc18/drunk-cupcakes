548ce5e4eacf0638b67afca5d7b00b05c73398b7
kaydc18 <klattk@gmail.com>
2020-11-30 14:08:25 -0500

    update form

--- app/javascript/react/components/ReviewForm.js
+++ app/javascript/react/components/ReviewForm.js
@@ -7,13 +7,8 @@ const ReviewForm = (props) => {
     suggested_edits: ""
   })
 
-  let err
+  
   const handleInputChange = event => {
-    if (event.currentTarget.name === "rating") {
-      if (event.currentTarget.value != "" && !Number(event.currentTarget.value)) {
-        err = <h3>Your rating must be a number</h3>
-      }
-    }
     setNewReview({
       ...newReview,
       [event.currentTarget.name]: event.currentTarget.value
@@ -33,37 +28,36 @@ const ReviewForm = (props) => {
       </div>
 
        <form onSubmit={onSubmitHandler}>
-         {err}
-       <div className="grid-container">
-        <div className="grid-x grid-padding-x">
-         <div className="cell large-12">
-          <label>
-            How would you rate this recipe (1-5)?
-            <input type="number"  min="1"
-        max="5" name="rating" onChange={handleInputChange} />
-          </label>
-         </div>
+        <div className="grid-container">
+          <div className="grid-x grid-padding-x">
+          <div className="cell large-12">
+            <label>
+              How would you rate this recipe (1-5)?
+              <input type="number"  min="1"
+          max="5" name="rating" onChange={handleInputChange} />
+            </label>
+          </div>
 
 
-        <div className="cell large-12">
+          <div className="cell large-12">
+            <label>
+              What are your thoughts on this recipe?
+              <textarea type="text" name="thoughts" onChange={handleInputChange} placeholder="Enter text here" />
+            </label>
+          </div>
+
+          <div className="cell large-12">
           <label>
-            What are your thoughts on this recipe?
-            <textarea type="text" name="thoughts" onChange={handleInputChange} placeholder="Enter text here" />
+            How would you change this recipe?
+            <textarea type="text" name="suggested_edits" onChange={handleInputChange} placeholder="Enter text here" />
           </label>
+          </div>
+
+          <div className="cell large-12">
+          <input className="button" type="submit" value="Submit" />
+          </div>
+          </div>
         </div>
-
-        <div className="cell large-12">
-         <label>
-           How would you change this recipe?
-           <textarea type="text" name="suggested_edits" onChange={handleInputChange} placeholder="Enter text here" />
-         </label>
-         </div>
-
-         <div className="cell large-12">
-         <input className="button" type="submit" value="Submit" />
-         </div>
-         </div>
-         </div>
        </form>
      </div>
 
